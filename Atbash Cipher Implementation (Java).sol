625303e9ee44d9eccd425b12577b52c58c85ae6b
rajmahesh446-cell <rajmahesh446@gmail.com>
2026-02-05 11:28:35 +0530

    Update Atbash Cipher Implementation (Java).sol

--- Atbash Cipher Implementation (Java).sol
+++ Atbash Cipher Implementation (Java).sol
@@ -37,11 +37,7 @@ public class AtbashCipher {
         // Example 4: Mixed Characters
         System.out.println("4. PYRAMID  -> " + applyAtbash("PYRAMID"));
         
-        
-
-
-
-        // Example 5: Numbers (Unchanged)
+       // Example 5: Numbers (Unchanged)
         System.out.println("5. A1B2C3   -> " + applyAtbash("A1B2C3"));
     }
 }
